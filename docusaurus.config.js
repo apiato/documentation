182af8d392a126bafbf16274f283b2b0f2df9b16
Moslem Deris <moslem.deris@gmail.com>
2023-03-07 19:45:12 +0330

    refactor: change files with breaking change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,160 +1,183 @@
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'Apiato',
-  tagline: 'Build scalable API\'s faster | With PHP and Laravel',
-  url: 'http://apiato.io/',
+  tagline: "Build scalable APIs faster | With PHP and Laravel",
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://apiato.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'apiato', // Usually your GitHub org/user name.
+  projectName: 'documentation', // Usually your repo name.
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'apiato',
-  projectName: 'documentation',
-  stylesheets: [
-    "https://fonts.googleapis.com/icon?family=Material+Icons",
-  ],
-  themeConfig: {
-    hideableSidebar: true,
-    announcementBar: {
-      id: 'announcementBar-2', // Increment on change
-      content: `⭐️ If you like Apiato, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/apiato/apiato">GitHub</a>`,
-    },
-    navbar: {
-      title: 'Apiato',
-      logo: {
-        alt: 'Apiato Logo',
-        src: 'img/logo.png',
-      },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {
-          type: 'docsVersionDropdown',
-          position: 'right',
-          dropdownActiveClassDisabled: true,
-          dropdownItemsAfter: [
-            // {
-            //   to: '/versions',
-            //   label: 'All versions',
-            // },
-          ],
-        },
-        // {
-        //   type: 'localeDropdown',
-        //   position: 'right',
-        //   dropdownItemsAfter: [
-        //     {
-        //       to: 'https://github.com/moslem-deris/docs/issues',
-        //       label: 'Help Us Translate',
-        //     },
-        //   ],
-        // },
-        {
-          href: 'https://github.com/apiato/apiato',
-          position: 'right',
-          className: 'header-github-link',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started',
-              to: '/docs',
-            },
-            {
-              label: 'Porto Architecture',
-              to: 'https://github.com/Mahmoudz/Porto#porto-software-architectural-pattern',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Discord Server',
-              href: 'https://discord.gg/ryPcV4KM5k',
-            },
-            {
-              label: 'GitHub Issues',
-              href: 'https://github.com/apiato/apiato/issues',
-            },
-          ],
-        },
-        {
-          title: 'Contributors',
-          items: [
-            {
-              label: 'Contribution Guide',
-              href: '/docs/contribution-guide',
-            },
-            {
-              label: 'Code of Conduct',
-              href: 'https://github.com/apiato/apiato/blob/9.0/CODE_OF_CONDUCT.md',
-            },
-          ],
-        },
-        {
-          title: 'Sponsors',
-          items: [
-            {
-              label: 'Our Sponsors',
-              href: 'https://github.com/apiato/apiato#sponsors',
-            },
-            {
-              label: 'Become a Sponsor',
-              href: 'https://opencollective.com/apiato',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © 2016 - ${new Date().getFullYear()}`,
-    },
-    prism: {
-      additionalLanguages: ['php'],
-      theme: require('prism-react-renderer/themes/github'),
-      darkTheme: require('prism-react-renderer/themes/dracula'),
-    },
-    googleAnalytics: {
-      trackingID: 'UA-37514928-10',
-      anonymizeIP: true,
-    },
-    algolia: {
-      // Apiato DocSearch Config File @ https://github.com/algolia/docsearch-configs/blob/master/configs/apiato.json
-      apiKey: '5a14b34a43f1bba00c8f096d48577fa8',
-      indexName: 'apiato',
-      contextualSearch: true,
-      searchParameters: {},
-    },
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
-  plugins: [],
+
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
+          breadcrumbs: true,
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/apiato/documentation/tree/master',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/apiato/documentation/tree/master',
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
-          versions:{
-            current: {
-              label: `Next 🚧`,
-            },
+          disableVersioning: false,
+          includeCurrentVersion: true,
+          versions: {
+              current: {
+                  label: `Next 🚧`,
+              },
           }
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      },
+      }),
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      announcementBar: {
+        id: 'announcementBar-2', // Increment on change
+        content: `⭐️ If you like Apiato, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/apiato/apiato">GitHub</a>`,
+      },
+      docs: {
+        sidebar: {
+          hideable: true
+        },
+      },
+      algolia: {
+        appId: 'OZKEC9E27Y',
+        apiKey: '94a1a71e8682dac72c8708fa75280e2a',
+        indexName: 'apiato',
+        contextualSearch: true,
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: false,
+        //... other Algolia params
+      },
+      // Replace with your project's social card
+      // image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Apiato',
+        logo: {
+          alt: 'Apiato Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'getting-started/requirements',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/apiato/apiato',
+            position: 'right',
+            className: 'header-github-link'
+          },
+          {
+            type: 'docsVersionDropdown',
+            position: 'right',
+            dropdownActiveClassDisabled: true,
+            dropdownItemsAfter: [
+              // {
+              //   to: '/versions',
+              //   label: 'All versions',
+              // },
+            ],
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting Started',
+                to: '/docs',
+              },
+              {
+                label: 'Porto Architecture',
+                to: 'https://github.com/Mahmoudz/Porto#porto-software-architectural-pattern',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord Server',
+                href: 'https://discord.gg/ryPcV4KM5k',
+              },
+              {
+                label: 'GitHub Issues',
+                href: 'https://github.com/apiato/apiato/issues',
+              },
+            ],
+          },
+          {
+            title: 'Contributors',
+            items: [
+              {
+                label: 'Contribution Guide',
+                href: '/docs/contribution-guide',
+              },
+              {
+                label: 'Code of Conduct',
+                href: 'https://github.com/apiato/apiato/blob/9.0/CODE_OF_CONDUCT.md',
+              },
+            ],
+          },
+          {
+            title: 'Sponsors',
+            items: [
+              {
+                label: 'Our Sponsors',
+                href: 'https://github.com/apiato/apiato#sponsors',
+              },
+              {
+                label: 'Become a Sponsor',
+                href: 'https://opencollective.com/apiato',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © 2016 - ${new Date().getFullYear()}`,
+      },
+      prism: {
+        additionalLanguages: ['php'],
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
+
+module.exports = config;
